ee08301d946a35e37d9f20c96a62e4152b13cfa0
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-05-22 18:07:30 -0400

    Update AssemblyInfo.vb

--- guinget/My Project/AssemblyInfo.vb
+++ guinget/My Project/AssemblyInfo.vb
@@ -9,10 +9,10 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("guinget")>
-<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyDescription("Unofficial GUI for Microsoft's Windows Package Manager (winget). Windows and all related words are copyright and trademark Microsoft Corporation.")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("guinget")>
-<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyCopyright("Copyright (C) 2020 Drew Naylor. Licensed under Apache License 2.0.")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("0.1.0.0")>
+<Assembly: AssemblyFileVersion("0.1.0.0")>
