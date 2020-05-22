a3559f31e04c9c9e65b806c31affffd434ce9458
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-05-22 17:59:31 -0400

    Enable HiDPI auto-resizing + rename main form.

--- guinget/MainWindow.Designer.vb
+++ guinget/MainWindow.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class aaformMainWindow
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,17 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.SuspendLayout()
+        '
+        'aaformMainWindow
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(120.0!, 120.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.ClientSize = New System.Drawing.Size(282, 253)
+        Me.Name = "aaformMainWindow"
+        Me.Text = "guinget"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
--- guinget/My Project/Application.Designer.vb
+++ guinget/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.guinget.Form1
+            Me.MainForm = Global.guinget.aaformMainWindow
         End Sub
     End Class
 End Namespace
