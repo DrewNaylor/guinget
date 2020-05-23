610dd495a04bde06dc4f4f479d27da0238a70b98
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-05-23 03:54:44 -0400

    Add boilerplate.

--- guinget/MainWindow.vb
+++ guinget/MainWindow.vb
@@ -1,4 +1,28 @@
-﻿Public Class aaformMainWindow
+﻿' guinget - Unofficial GUI for Microsoft's Windows Package Manager (winget)
+' Copyright (C) 2020 Drew Naylor
+' (Note that the copyright years include the years left out by the hyphen.)
+' winget, Windows, and all related words are copyright and trademark Microsoft Corporation.
+'
+' This file is a part of the guinget project.
+' Neither guinget nor Drew Naylor are associated with Microsoft
+' and Microsoft does not approve of guinget.
+'
+'
+'   Licensed under the Apache License, Version 2.0 (the "License");
+'   you may not use this file except in compliance with the License.
+'   You may obtain a copy of the License at
+'
+'     http://www.apache.org/licenses/LICENSE-2.0
+'
+'   Unless required by applicable law or agreed to in writing, software
+'   distributed under the License is distributed on an "AS IS" BASIS,
+'   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+'   See the License for the specific language governing permissions and
+'   limitations under the License.
+
+
+
+Public Class aaformMainWindow
     Private Sub ExamplePackageToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExamplePackageToolStripMenuItem.Click
         ' Original code for info as well as the PackageInfo class was from this StackOverflow answer:
         ' https://stackoverflow.com/a/36801744
