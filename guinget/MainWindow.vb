ca042aff9b7b90ff9c93685b3bb752368152b9f3
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-05-23 16:42:44 -0400

    Make sure only selected rows show package details.
    This allows multiple rows to be selected, then having all other rows deselected except for one the user doesn't interact with. This last row that's still selected is what's looked up for package details.

--- guinget/MainWindow.vb
+++ guinget/MainWindow.vb
@@ -85,7 +85,7 @@ Public Class aaformMainWindow
             ' If only one is selected, get its details into the details textbox.
             ' Set the textbox to say "Loading..." so it doesn't look like it's hanging.
             textboxPackageDetails.Text = "Loading, please wait..."
-            textboxPackageDetails.Text = Await libguinget.PackageTools.GetPkgDetailsAsync(datagridviewPackageList.Item(2, datagridviewPackageList.CurrentRow.Index).Value.ToString)
+            textboxPackageDetails.Text = Await libguinget.PackageTools.GetPkgDetailsAsync(datagridviewPackageList.Item(2, datagridviewPackageList.SelectedRows.Item(0).Index).Value.ToString)
         End If
     End Sub
 
