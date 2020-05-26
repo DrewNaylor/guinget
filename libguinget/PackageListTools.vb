ce8193a6b5c5e1f83a61b68cbc4c8fc7408004f0
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-05-26 13:06:09 -0400

    Add comment.

--- libguinget/PackageListTools.vb
+++ libguinget/PackageListTools.vb
@@ -39,6 +39,8 @@ Public Class PackageListTools
         ' Create a variable to hold the folder we'll be looking in.
         Dim ManifestAppDataFolder As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\winget-frontends\pkglist\manifests"
 
+        ' Take the Id string for each package file and append it to the
+        ' package list array variable.
         For Each PackageManifest As String In My.Computer.FileSystem.GetFiles(ManifestAppDataFolder, FileIO.SearchOption.SearchAllSubDirectories, "*.yaml")
             MessageBox.Show(PackageManifest)
         Next
