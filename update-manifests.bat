b5ba021e7567beb8cbdc750a5fc6bfd98d74d37c
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-05-27 04:36:56 -0400

    Updating some of the beginning output.

--- update-manifests.bat
+++ update-manifests.bat
@@ -26,15 +26,17 @@ title %REGULAR_TITLE_BAR%
 
 
 :beginning-of-script
-echo UXL Autobuild Script (or UAS) is provided to others free of charge and without warranty by Drew Naylor.
-echo UAS Version %VERSIONNUMBER% Copyright 2016-2018 Drew Naylor.
+echo update-manifests is provided to others free of charge and without warranty by Drew Naylor.
+echo Based on UXL Autobuild Script version 2018.01-1:
+echo https://gist.github.com/DrewNaylor/22e3f1cded702fff494a46dabe643fde
+echo update-manifests Version %VERSIONNUMBER% Copyright 2020 Drew Naylor.
 echo Drew Naylor has no relation to Microsoft or GitHub.
-echo UAS is not to be confused with other projects and software with a similar name.
+echo update-manifests is related to neither Microsoft nor winget, and Microsoft does not endorse this software.
 echo If anything happens to your computer, Drew Naylor is not responsible. Use at your own risk.
-echo Please visit https://github.com/DrewNaylor/UXL-Launcher/ for more information on UXL Launcher which is open source.
+echo Please visit https://github.com/DrewNaylor/guinget/ for more information on this script's parent project, guinget, which is open source.
 echo(
 
-echo The purpose of UAS is to expediate the downloading, extracting, and building of UXL Launcher for developers or those who want to test the latest changes as soon as possible. Not recommended for end users who want stable software. UAS will use PowerShell to download a Zip file from github.com, extract it into a temp file in your user profile folder with PowerShell, and build the Visual Basic project file with MSBuild. 
+echo The purpose of update-manifests is to expediate the downloading and extracting of winget sources from Microsoft's Community Package Repository. For now, this is the only source supported, but the goal is to support more sources in the future, along with winget gaining that feature. UAS will use PowerShell to download a Zip file from github.com, extract it into a temp file in your user profile folder with PowerShell, and build the Visual Basic project file with MSBuild. 
 
 echo(
 echo Windows PowerShell, Microsoft Build Tools 2015 and Microsoft .Net Framework 4.6.1 are required to build and run this application. Links are available in the System Requirements in the README file here:  https://github.com/DrewNaylor/UXL-Launcher/blob/master/README.md#system-requirements
