f0a7aa00a2aaaa3347493852eda2eae0d8c01c54
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-05-28 23:46:25 -0400

    Add table layout panel to apply changes window + resize...
    window.

--- guinget/ApplyChangesWindow.Designer.vb
+++ guinget/ApplyChangesWindow.Designer.vb
@@ -22,16 +22,34 @@ Partial Class ApplyChangesWindow
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(690, 450)
+        Me.TableLayoutPanel1.TabIndex = 0
         '
         'ApplyChangesWindow
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(690, 450)
+        Me.Controls.Add(Me.TableLayoutPanel1)
         Me.Name = "ApplyChangesWindow"
         Me.Text = "ApplyChangesWindow"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
 End Class
