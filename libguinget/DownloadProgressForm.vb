7c7ac1a421421ab60c35b19f7114cff0fdf299c2
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-06-26 15:55:44 -0400

    Set up package list url property.

--- libguinget/DownloadProgressForm.vb
+++ libguinget/DownloadProgressForm.vb
@@ -4,7 +4,16 @@
     ' and display progress:
     ' https://stackoverflow.com/a/19459595
 
+
+    ' Get and return property for the package list URL.
     Friend Shared Property PackageListUrl As String
+        Get
+            Return _PackageListUrl
+        End Get
+        Set(value As String)
+            _PackageListUrl = value
+        End Set
+    End Property
 
     ' Package list URL for the property.
     Private Shared _PackageListUrl As String = String.Empty
