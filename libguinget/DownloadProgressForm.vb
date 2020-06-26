45d067406780bf32a394c295abb5356251fd860f
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-06-26 16:00:35 -0400

    Set label text.

--- libguinget/DownloadProgressForm.vb
+++ libguinget/DownloadProgressForm.vb
@@ -4,6 +4,14 @@
     ' and display progress:
     ' https://stackoverflow.com/a/19459595
 
+    Private Sub DownloadProgressForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Set label text.
+        labelSourceName.Text = labelSourceLocation.Text & PackageListSourceName
+        labelSourceLocation.Text = labelSourceLocation.Text & PackageListUrl
+
+    End Sub
+
 #Region "Package list properties"
     ' Package list URL.
     Friend Shared Property PackageListUrl As String = String.Empty
