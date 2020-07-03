ca4b7764160370b83881de0e87413acc694a8398
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-07-03 00:10:21 -0400

    Don't show the context menu if we're double-clicking in the...
    header. Fixes #16.

--- guinget/MainWindow.vb
+++ guinget/MainWindow.vb
@@ -443,7 +443,16 @@ Public Class aaformMainWindow
 
     Private Sub datagridviewPackageList_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles datagridviewPackageList.CellDoubleClick
         ' Show package context menu on cell double-click, like Synaptic.
-        contextmenustripPackageMenu.Show(MousePosition)
+
+        If e.ColumnIndex >= 0 AndAlso e.RowIndex >= 0 Then
+            ' Make sure we're not double-clicking on the column headers
+            ' before showing the context menu. This allows quick column
+            ' auto-sizing based on cell contents when double-clicking
+            ' the header separators.
+            ' Related issue:
+            ' https://github.com/DrewNaylor/guinget/issues/16
+            contextmenustripPackageMenu.Show(MousePosition)
+        End If
     End Sub
 
     Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
