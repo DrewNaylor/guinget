4d5c92b7ce9a7f257a0ec290a8ba843311f6c0de
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-08-15 19:38:29 -0400

    Make the directory again after deleting it.

--- postbuild.bat
+++ postbuild.bat
@@ -4,6 +4,7 @@ REM Moves libraries into their own folder.
 REM Check if lib folder exists first.
 IF EXIST guinget\bin\Debug\lib\ (
 rmdir /s /q guinget\bin\Debug\lib
+mkdir guinget\bin\Debug\lib
 GOTO STARTMOVE
 ) ELSE (
 mkdir guinget\bin\Debug\lib
