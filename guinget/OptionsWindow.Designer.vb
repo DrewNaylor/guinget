900e87ca71353c8a30bd578e0014b1eb9bba2bb4
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-08-31 19:52:18 -0400

    Name controls.

--- guinget/OptionsWindow.Designer.vb
+++ guinget/OptionsWindow.Designer.vb
@@ -22,93 +22,93 @@ Partial Class OptionsWindow
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.TabControl1 = New System.Windows.Forms.TabControl()
-        Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TableLayoutPanel1.SuspendLayout()
-        Me.TabControl1.SuspendLayout()
+        Me.tablelayoutpanelOptions = New System.Windows.Forms.TableLayoutPanel()
+        Me.buttonDefaults = New System.Windows.Forms.Button()
+        Me.buttonOk = New System.Windows.Forms.Button()
+        Me.buttonCancel = New System.Windows.Forms.Button()
+        Me.tabcontrolOptions = New System.Windows.Forms.TabControl()
+        Me.tabpageSearch = New System.Windows.Forms.TabPage()
+        Me.tablelayoutpanelOptions.SuspendLayout()
+        Me.tabcontrolOptions.SuspendLayout()
         Me.SuspendLayout()
         '
-        'TableLayoutPanel1
+        'tablelayoutpanelOptions
         '
-        Me.TableLayoutPanel1.ColumnCount = 4
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 106.0!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 106.0!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 106.0!))
-        Me.TableLayoutPanel1.Controls.Add(Me.Button1, 0, 1)
-        Me.TableLayoutPanel1.Controls.Add(Me.Button3, 3, 1)
-        Me.TableLayoutPanel1.Controls.Add(Me.Button2, 2, 1)
-        Me.TableLayoutPanel1.Controls.Add(Me.TabControl1, 0, 0)
-        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
-        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
-        Me.TableLayoutPanel1.RowCount = 2
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.94991!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.050093!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(465, 539)
-        Me.TableLayoutPanel1.TabIndex = 0
+        Me.tablelayoutpanelOptions.ColumnCount = 4
+        Me.tablelayoutpanelOptions.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 106.0!))
+        Me.tablelayoutpanelOptions.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.tablelayoutpanelOptions.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 106.0!))
+        Me.tablelayoutpanelOptions.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 106.0!))
+        Me.tablelayoutpanelOptions.Controls.Add(Me.buttonDefaults, 0, 1)
+        Me.tablelayoutpanelOptions.Controls.Add(Me.buttonCancel, 3, 1)
+        Me.tablelayoutpanelOptions.Controls.Add(Me.buttonOk, 2, 1)
+        Me.tablelayoutpanelOptions.Controls.Add(Me.tabcontrolOptions, 0, 0)
+        Me.tablelayoutpanelOptions.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tablelayoutpanelOptions.Location = New System.Drawing.Point(0, 0)
+        Me.tablelayoutpanelOptions.Name = "tablelayoutpanelOptions"
+        Me.tablelayoutpanelOptions.RowCount = 2
+        Me.tablelayoutpanelOptions.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.94991!))
+        Me.tablelayoutpanelOptions.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.050093!))
+        Me.tablelayoutpanelOptions.Size = New System.Drawing.Size(465, 539)
+        Me.tablelayoutpanelOptions.TabIndex = 0
         '
-        'Button1
+        'buttonDefaults
         '
-        Me.Button1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Button1.Location = New System.Drawing.Point(3, 503)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(100, 33)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Defaults"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.buttonDefaults.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.buttonDefaults.Location = New System.Drawing.Point(3, 503)
+        Me.buttonDefaults.Name = "buttonDefaults"
+        Me.buttonDefaults.Size = New System.Drawing.Size(100, 33)
+        Me.buttonDefaults.TabIndex = 1
+        Me.buttonDefaults.Text = "Defaults"
+        Me.buttonDefaults.UseVisualStyleBackColor = True
         '
-        'Button2
+        'buttonOk
         '
-        Me.Button2.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Button2.Location = New System.Drawing.Point(256, 503)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(100, 33)
-        Me.Button2.TabIndex = 4
-        Me.Button2.Text = "OK"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.buttonOk.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.buttonOk.Location = New System.Drawing.Point(256, 503)
+        Me.buttonOk.Name = "buttonOk"
+        Me.buttonOk.Size = New System.Drawing.Size(100, 33)
+        Me.buttonOk.TabIndex = 4
+        Me.buttonOk.Text = "OK"
+        Me.buttonOk.UseVisualStyleBackColor = True
         '
-        'Button3
+        'buttonCancel
         '
-        Me.Button3.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Button3.Location = New System.Drawing.Point(362, 503)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(100, 33)
-        Me.Button3.TabIndex = 5
-        Me.Button3.Text = "Cancel"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.buttonCancel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.buttonCancel.Location = New System.Drawing.Point(362, 503)
+        Me.buttonCancel.Name = "buttonCancel"
+        Me.buttonCancel.Size = New System.Drawing.Size(100, 33)
+        Me.buttonCancel.TabIndex = 5
+        Me.buttonCancel.Text = "Cancel"
+        Me.buttonCancel.UseVisualStyleBackColor = True
         '
-        'TabControl1
+        'tabcontrolOptions
         '
-        Me.TableLayoutPanel1.SetColumnSpan(Me.TabControl1, 4)
-        Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TabControl1.Location = New System.Drawing.Point(3, 3)
-        Me.TabControl1.Name = "TabControl1"
-        Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(459, 494)
-        Me.TabControl1.TabIndex = 6
+        Me.tablelayoutpanelOptions.SetColumnSpan(Me.tabcontrolOptions, 4)
+        Me.tabcontrolOptions.Controls.Add(Me.tabpageSearch)
+        Me.tabcontrolOptions.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tabcontrolOptions.Location = New System.Drawing.Point(3, 3)
+        Me.tabcontrolOptions.Name = "tabcontrolOptions"
+        Me.tabcontrolOptions.SelectedIndex = 0
+        Me.tabcontrolOptions.Size = New System.Drawing.Size(459, 494)
+        Me.tabcontrolOptions.TabIndex = 6
         '
-        'TabPage1
+        'tabpageSearch
         '
-        Me.TabPage1.Location = New System.Drawing.Point(4, 25)
-        Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(451, 465)
-        Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "TabPage1"
-        Me.TabPage1.UseVisualStyleBackColor = True
+        Me.tabpageSearch.Location = New System.Drawing.Point(4, 25)
+        Me.tabpageSearch.Name = "tabpageSearch"
+        Me.tabpageSearch.Padding = New System.Windows.Forms.Padding(3)
+        Me.tabpageSearch.Size = New System.Drawing.Size(451, 465)
+        Me.tabpageSearch.TabIndex = 0
+        Me.tabpageSearch.Text = "Search"
+        Me.tabpageSearch.UseVisualStyleBackColor = True
         '
         'OptionsWindow
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(120.0!, 120.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
         Me.ClientSize = New System.Drawing.Size(465, 539)
-        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.tablelayoutpanelOptions)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -117,16 +117,16 @@ Partial Class OptionsWindow
         Me.ShowInTaskbar = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Options"
-        Me.TableLayoutPanel1.ResumeLayout(False)
-        Me.TabControl1.ResumeLayout(False)
+        Me.tablelayoutpanelOptions.ResumeLayout(False)
+        Me.tabcontrolOptions.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents TabControl1 As TabControl
-    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents tablelayoutpanelOptions As TableLayoutPanel
+    Friend WithEvents buttonDefaults As Button
+    Friend WithEvents buttonCancel As Button
+    Friend WithEvents buttonOk As Button
+    Friend WithEvents tabcontrolOptions As TabControl
+    Friend WithEvents tabpageSearch As TabPage
 End Class
