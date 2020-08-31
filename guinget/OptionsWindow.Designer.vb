ad5365164da8c34f3aac43aafd98c0417ccdc017
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-08-31 19:49:07 -0400

    Remove second tabpage.

--- guinget/OptionsWindow.Designer.vb
+++ guinget/OptionsWindow.Designer.vb
@@ -28,7 +28,6 @@ Partial Class OptionsWindow
         Me.Button3 = New System.Windows.Forms.Button()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.TableLayoutPanel1.SuspendLayout()
         Me.TabControl1.SuspendLayout()
         Me.SuspendLayout()
@@ -87,7 +86,6 @@ Partial Class OptionsWindow
         '
         Me.TableLayoutPanel1.SetColumnSpan(Me.TabControl1, 4)
         Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Controls.Add(Me.TabPage2)
         Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.TabControl1.Location = New System.Drawing.Point(3, 3)
         Me.TabControl1.Name = "TabControl1"
@@ -104,16 +102,6 @@ Partial Class OptionsWindow
         Me.TabPage1.TabIndex = 0
         Me.TabPage1.Text = "TabPage1"
         Me.TabPage1.UseVisualStyleBackColor = True
-        '
-        'TabPage2
-        '
-        Me.TabPage2.Location = New System.Drawing.Point(4, 25)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(92, 71)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "TabPage2"
-        Me.TabPage2.UseVisualStyleBackColor = True
         '
         'OptionsWindow
         '
@@ -141,5 +129,4 @@ Partial Class OptionsWindow
     Friend WithEvents Button2 As Button
     Friend WithEvents TabControl1 As TabControl
     Friend WithEvents TabPage1 As TabPage
-    Friend WithEvents TabPage2 As TabPage
 End Class
