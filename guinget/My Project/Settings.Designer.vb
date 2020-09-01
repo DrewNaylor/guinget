1b2bc159d39613c67de930ec4b0071c50c084fa7
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-08-31 21:34:57 -0400

    If this is True and the files are installed to the same...
    location as the previous version, it'll upgrade the settings automatically. Fixes #36.

--- guinget/My Project/Settings.Designer.vb
+++ guinget/My Project/Settings.Designer.vb
@@ -164,7 +164,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
         Public Property UpgradeSettingsFromPreviousVersion() As Boolean
             Get
                 Return CType(Me("UpgradeSettingsFromPreviousVersion"),Boolean)
