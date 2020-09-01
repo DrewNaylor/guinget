6b8f819210ebab23ae6d214c00215a028def915d
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-08-31 20:38:27 -0400

    Use sidebar visibility code for another part.

--- guinget/MainWindow.vb
+++ guinget/MainWindow.vb
@@ -612,13 +612,7 @@ Public Class aaformMainWindow
         comboboxSidebarTabSelector.SelectedIndex = 0
 
         ' Show or hide the sidebar.
-        If My.Settings.ShowSidebar = True Then
-            splitcontainerSidebarAndPkgList.Panel1Collapsed = False
-            SidebarToolStripMenuItem.Checked = True
-        Else
-            splitcontainerSidebarAndPkgList.Panel1Collapsed = True
-            SidebarToolStripMenuItem.Checked = False
-        End If
+        SidebarVisibility(My.Settings.ShowSidebar)
 
         ' Hide unfinished controls on startup if My.Settings.HideUnfinishedControls = True.
         If My.Settings.HideUnfinishedControls = True Then
