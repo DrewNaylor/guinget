83ad50b6872dd9117a4f3411c5388b6506877fb4
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-09-04 14:58:54 -0400

    Show sidebar by default.

--- guinget/My Project/Settings.Designer.vb
+++ guinget/My Project/Settings.Designer.vb
@@ -140,7 +140,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
         Public Property ShowSidebar() As Boolean
             Get
                 Return CType(Me("ShowSidebar"),Boolean)
