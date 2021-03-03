613fc7928e347e3f3328efc37746e208579a4a7d
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-03-03 05:30:11 -0500

    Re-arranging some stuff + adding checkboxes for the settings...
    to specify whether version numbers are specified.

--- guinget/MiniApplyChangesOptionsForm.Designer.vb
+++ guinget/MiniApplyChangesOptionsForm.Designer.vb
@@ -28,12 +28,18 @@ Partial Class MiniApplyChangesOptionsForm
         Me.checkboxInstallInteractively = New System.Windows.Forms.CheckBox()
         Me.checkboxOpenAutomatically = New System.Windows.Forms.CheckBox()
         Me.buttonClose = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'labelWhyElevate
         '
         Me.labelWhyElevate.AutoSize = True
-        Me.labelWhyElevate.Location = New System.Drawing.Point(29, 52)
+        Me.labelWhyElevate.Location = New System.Drawing.Point(20, 43)
         Me.labelWhyElevate.Name = "labelWhyElevate"
         Me.labelWhyElevate.Size = New System.Drawing.Size(287, 65)
         Me.labelWhyElevate.TabIndex = 5
@@ -42,7 +48,7 @@ Partial Class MiniApplyChangesOptionsForm
         'checkboxElevateWinget
         '
         Me.checkboxElevateWinget.AutoSize = True
-        Me.checkboxElevateWinget.Location = New System.Drawing.Point(12, 35)
+        Me.checkboxElevateWinget.Location = New System.Drawing.Point(3, 26)
         Me.checkboxElevateWinget.Margin = New System.Windows.Forms.Padding(3, 3, 3, 0)
         Me.checkboxElevateWinget.Name = "checkboxElevateWinget"
         Me.checkboxElevateWinget.Size = New System.Drawing.Size(143, 17)
@@ -53,7 +59,7 @@ Partial Class MiniApplyChangesOptionsForm
         'checkboxInstallInteractively
         '
         Me.checkboxInstallInteractively.AutoSize = True
-        Me.checkboxInstallInteractively.Location = New System.Drawing.Point(12, 12)
+        Me.checkboxInstallInteractively.Location = New System.Drawing.Point(3, 3)
         Me.checkboxInstallInteractively.Name = "checkboxInstallInteractively"
         Me.checkboxInstallInteractively.Size = New System.Drawing.Size(153, 17)
         Me.checkboxInstallInteractively.TabIndex = 3
@@ -64,7 +70,7 @@ Partial Class MiniApplyChangesOptionsForm
         '
         Me.checkboxOpenAutomatically.AutoSize = True
         Me.checkboxOpenAutomatically.CheckAlign = System.Drawing.ContentAlignment.TopLeft
-        Me.checkboxOpenAutomatically.Location = New System.Drawing.Point(12, 139)
+        Me.checkboxOpenAutomatically.Location = New System.Drawing.Point(9, 270)
         Me.checkboxOpenAutomatically.Name = "checkboxOpenAutomatically"
         Me.checkboxOpenAutomatically.Size = New System.Drawing.Size(294, 30)
         Me.checkboxOpenAutomatically.TabIndex = 6
@@ -74,34 +80,88 @@ Partial Class MiniApplyChangesOptionsForm
         'buttonClose
         '
         Me.buttonClose.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.buttonClose.Location = New System.Drawing.Point(267, 191)
+        Me.buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.buttonClose.Location = New System.Drawing.Point(259, 306)
         Me.buttonClose.Name = "buttonClose"
         Me.buttonClose.Size = New System.Drawing.Size(86, 28)
         Me.buttonClose.TabIndex = 7
         Me.buttonClose.Text = "Close"
         Me.buttonClose.UseVisualStyleBackColor = True
         '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(23, 158)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(105, 17)
+        Me.CheckBox1.TabIndex = 8
+        Me.CheckBox1.Text = "When upgrading"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(23, 182)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(110, 17)
+        Me.CheckBox2.TabIndex = 9
+        Me.CheckBox2.Text = "When uninstalling"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(23, 135)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(98, 17)
+        Me.CheckBox3.TabIndex = 10
+        Me.CheckBox3.Text = "When installing"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.CheckBox3)
+        Me.Panel1.Controls.Add(Me.CheckBox1)
+        Me.Panel1.Controls.Add(Me.checkboxInstallInteractively)
+        Me.Panel1.Controls.Add(Me.CheckBox2)
+        Me.Panel1.Controls.Add(Me.checkboxElevateWinget)
+        Me.Panel1.Controls.Add(Me.labelWhyElevate)
+        Me.Panel1.Controls.Add(Me.buttonClose)
+        Me.Panel1.Controls.Add(Me.checkboxOpenAutomatically)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(348, 337)
+        Me.Panel1.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 119)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(126, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Specify version number..."
+        '
         'MiniApplyChangesOptionsForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
-        Me.AutoScroll = True
         Me.CancelButton = Me.buttonClose
-        Me.ClientSize = New System.Drawing.Size(358, 224)
-        Me.Controls.Add(Me.buttonClose)
-        Me.Controls.Add(Me.checkboxOpenAutomatically)
-        Me.Controls.Add(Me.labelWhyElevate)
-        Me.Controls.Add(Me.checkboxElevateWinget)
-        Me.Controls.Add(Me.checkboxInstallInteractively)
+        Me.ClientSize = New System.Drawing.Size(348, 337)
+        Me.Controls.Add(Me.Panel1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
-        Me.MinimumSize = New System.Drawing.Size(374, 263)
+        Me.MinimumSize = New System.Drawing.Size(351, 346)
         Me.Name = "MiniApplyChangesOptionsForm"
         Me.ShowIcon = False
         Me.ShowInTaskbar = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
         Me.Text = "Options: Apply Changes"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
@@ -110,4 +170,9 @@ Partial Class MiniApplyChangesOptionsForm
     Friend WithEvents checkboxInstallInteractively As CheckBox
     Friend WithEvents checkboxOpenAutomatically As CheckBox
     Friend WithEvents buttonClose As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
 End Class
