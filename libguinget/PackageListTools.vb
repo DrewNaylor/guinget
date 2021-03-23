54daa3e3623d479a288c716e1629df042aaca4f0
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-03-22 23:56:31 -0400

    Replacement string might be getting better.

--- libguinget/PackageListTools.vb
+++ libguinget/PackageListTools.vb
@@ -568,20 +568,21 @@ Public Class PackageListTools
 
         ' If we can't do a simple replacement on each "." in the ID,
         ' we'll have to fall back to the slower method.
-        Dim QuickPathReplaceReplaceAllPeriods As String = ManifestAppDataFolder & "\" & ManifestId.Substring(0, 1).ToLowerInvariant & "\" & ManifestId.Replace(".", "\") & "\" & ManifestVersion & ".yaml"
+        Dim QuickPathReplaceReplaceAllPeriods As String = ManifestAppDataFolder & "\" & ManifestId.Replace(".", "\") & "\" & ManifestVersion & ".yaml"
         ' Replacing only the first period uses code based on this:
         ' https://stackoverflow.com/a/5015804
-        Dim QuickPathReplaceReplaceOnlyFirstPeriod As String = ManifestAppDataFolder & "\" & ManifestId.Insert(ManifestId.IndexOf("."), "\").Remove(ManifestId.IndexOf(".")) & "\" & ManifestVersion & ".yaml"
+        Dim QuickPathReplaceReplaceOnlyFirstPeriod As String = ManifestAppDataFolder & "\" & ManifestId.Insert(ManifestId.IndexOf("."), "\").Remove(ManifestId.IndexOf("."), 1) & "\" & ManifestVersion & ".yaml"
 
         ' Show the new path.
         If ManifestId.ToLowerInvariant.Contains("gitter") Then
             MessageBox.Show(QuickPathReplaceReplaceOnlyFirstPeriod)
+            MessageBox.Show(QuickPathReplaceReplaceAllPeriods)
         End If
 
         If IO.File.Exists(QuickPathReplaceReplaceAllPeriods) Then
             Return QuickPathReplaceReplaceAllPeriods
 
-        ElseIf IO.File.Exists(QuickPathReplaceReplaceOnlyFirstPeriod) Then
+        ElseIf Not IO.File.Exists(QuickPathReplaceReplaceAllPeriods) And IO.File.Exists(QuickPathReplaceReplaceOnlyFirstPeriod) Then
             ' If we can't do the fastest one, try only replacing the first period.
             Return QuickPathReplaceReplaceOnlyFirstPeriod
 
