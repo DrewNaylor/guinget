a5c9676da61e2ae30ef40593fbeffb6c51e3cfc6
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-06-25 00:49:27 -0400

    Forgot to set the fast resize to be on by default.

--- guinget/My Project/Settings.Designer.vb
+++ guinget/My Project/Settings.Designer.vb
@@ -332,7 +332,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
         Public Property UseKDEStyleFastResize() As Boolean
             Get
                 Return CType(Me("UseKDEStyleFastResize"),Boolean)
