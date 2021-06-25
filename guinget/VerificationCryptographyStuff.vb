5c9783448de8b17107ea637c711ad8488bbe3e3f
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-06-25 02:29:37 -0400

    We need to do cryptography stuff to verify that the...
    manifest path is correct.

---
+++ guinget/VerificationCryptographyStuff.vb
@@ -0,0 +1,31 @@
+﻿' guinget - Unofficial GUI for Microsoft's Windows Package Manager (winget)
+'           Kinda like Synaptic, but for Windows. Not associated with either
+'           Microsoft or the Synaptic project. 
+' Copyright (C) 2020-2021 Drew Naylor
+' (Note that the copyright years include the years left out by the hyphen.)
+' winget, Windows, and all related words are copyright and trademark Microsoft Corporation.
+'
+' This file is a part of the guinget project.
+' Neither guinget nor Drew Naylor are associated with Microsoft
+' and Microsoft does not endorse guinget.
+'
+'
+'   Licensed under the Apache License, Version 2.0 (the "License");
+'   you may not use this file except in compliance with the License.
+'   You may obtain a copy of the License at
+'
+'     http://www.apache.org/licenses/LICENSE-2.0
+'
+'   Unless required by applicable law or agreed to in writing, software
+'   distributed under the License is distributed on an "AS IS" BASIS,
+'   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+'   See the License for the specific language governing permissions and
+'   limitations under the License.
+
+
+
+Imports System.Security.Cryptography
+Public Class VerificationCryptographyStuff
+
+
+End Class
