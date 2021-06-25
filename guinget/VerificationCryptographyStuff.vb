0c2ba82d8998226c4102ba46da1aabed73d07150
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-06-25 03:43:03 -0400

    Trying something else.

--- guinget/VerificationCryptographyStuff.vb
+++ guinget/VerificationCryptographyStuff.vb
@@ -58,15 +58,21 @@ Public Class VerificationCryptographyStuff
 
     Friend Shared Function GetUnciphered(InputString As String) As String
         ' Simple function to manually un-cipher a string.
-        InputString = InputString.Remove(InputString.IndexOf("e"), 0).Insert(InputString.IndexOf("e", 0), "Y")
-        InputString = InputString.Remove(InputString.IndexOf("e"), 1).Insert(InputString.IndexOf("e", 5), "r")
-        InputString = InputString.Remove(InputString.IndexOf("e"), 2).Insert(InputString.IndexOf("e", 12), "d")
+        InputString = InputString.Remove(InputString.IndexOf("e"), 0)
+        InputString = InputString.Insert(0, "Y")
+
+        InputString = InputString.Remove(InputString.IndexOf("e"), 1)
+        InputString = InputString.Insert(5, "r")
+
+        InputString = InputString.Remove(InputString.IndexOf("e"), 2)
+        InputString = InputString.Insert(12, "d")
+
         InputString = InputString.Remove(InputString.LastIndexOf("e")).Insert(InputString.LastIndexOf("e"), ")")
-        InputString = InputString.Remove(InputString.IndexOf("3"), 0).Insert(InputString.IndexOf("3", 0), "o")
-        InputString = InputString.Remove(InputString.IndexOf("3"), 1).Insert(InputString.IndexOf("3", 1), "e")
-        InputString = InputString.Remove(InputString.IndexOf("6"), 0).Insert(InputString.IndexOf("6", 0), "u")
-        InputString = InputString.Remove(InputString.IndexOf("b"), 0).Insert(InputString.IndexOf("b", 0), " ")
-        InputString = InputString.Remove(InputString.IndexOf("b"), 1).Insert(InputString.IndexOf("b", 1), ":")
+        InputString = InputString.Remove(InputString.IndexOf("3"), 1).Insert(InputString.IndexOf("3", 1), "o")
+        InputString = InputString.Remove(InputString.LastIndexOf("3")).Insert(InputString.LastIndexOf("3"), "e")
+        InputString = InputString.Remove(InputString.LastIndexOf("6")).Insert(InputString.IndexOf("6"), "u")
+        InputString = InputString.Remove(InputString.IndexOf("b"), 1).Insert(InputString.IndexOf("b", 4), " ")
+        InputString = InputString.Remove(InputString.LastIndexOf("b")).Insert(InputString.LastIndexOf("b"), ":")
         InputString = InputString.Remove(InputString.IndexOf("8"), 0).Insert(InputString.IndexOf("8", 0), "a")
 
         Return InputString.ToString
