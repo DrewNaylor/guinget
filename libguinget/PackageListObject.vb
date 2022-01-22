569e82cc0c002d334aeda95f07f94936bf75093d
Drew Naylor <DrewNaylor@users.noreply.github.com>
2022-01-22 01:38:30 -0500

    Begin switching to having the main window use a datatable as its...
    datasource.

---
+++ libguinget/PackageListObject.vb
@@ -0,0 +1,32 @@
+﻿' libguinget - Package and package list tools for guinget
+'              and other programs that want to use them.
+' Copyright (C) 2020-2022 Drew Naylor
+' (Note that the copyright years include the years left out by the hyphen.)
+' winget, Windows, and all related words are copyright and trademark Microsoft Corporation.
+'
+' This file is a part of the guinget project.
+' Neither guinget nor Drew Naylor are associated with Microsoft
+' and Microsoft does not endorse guinget.
+'
+'
+'   Licensed under the Apache License, Version 2.0 (the "License");
+'   you may not use this file except in compliance with the License.
+'   You may obtain a copy of the License at
+'
+'     http://www.apache.org/licenses/LICENSE-2.0
+'
+'   Unless required by applicable law or agreed to in writing, software
+'   distributed under the License is distributed on an "AS IS" BASIS,
+'   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+'   See the License for the specific language governing permissions and
+'   limitations under the License.
+
+
+
+Public Class PackageListObject
+
+    ' This is mainly just a publicly-available datatable used for the package list.
+    ' It'll probably have to also have its columns specified as well.
+    Public Shared PackageListTable As DataTable
+
+End Class
