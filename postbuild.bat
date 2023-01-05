a97d2d8afb535f4e293e0ed67cc2ef8e849f2af6
Drew Naylor <DrewNaylor@users.noreply.github.com>
2023-01-05 11:12:51 -0500

    Copy postbuild.bat over from the backports branch.

--- postbuild.bat
+++ postbuild.bat
@@ -1,16 +1,19 @@
 @echo off
 REM Moves libraries into their own folder.
 
+REM Define destination root dir.
+set "rootdir=Debug"
+
 REM Check if lib folder exists first.
-IF EXIST guinget\bin\Debug\lib\ (
+IF EXIST guinget\bin\%rootdir%\lib\ (
 REM If it exists, re-create the folder.
-rmdir /s /q guinget\bin\Debug\lib
-mkdir guinget\bin\Debug\lib
+rmdir /s /q guinget\bin\%rootdir%\lib
+mkdir guinget\bin\%rootdir%\lib
 REM Start moving.
 GOTO STARTMOVE
 ) ELSE (
 REM Folder doesn't exist, so create it.
-mkdir guinget\bin\Debug\lib
+mkdir guinget\bin\%rootdir%\lib
 REM Start moving.
 GOTO STARTMOVE
 )
@@ -18,10 +21,10 @@ GOTO STARTMOVE
 :STARTMOVE
 
 REM Move all DLL files in the bin\Debug directory into the "lib" folder.
-move guinget\bin\Debug\*.dll guinget\bin\Debug\lib\
+move guinget\bin\%rootdir%\*.dll guinget\bin\%rootdir%\lib\
 
 REM Move the "runtimes" folder into \lib.
-move guinget\bin\Debug\runtimes\* guinget\bin\Debug\lib\runtimes
+move guinget\bin\%rootdir%\runtimes\* guinget\bin\%rootdir%\lib\runtimes
 
 REM Exit the script so VS doesn't say we exited with code 1.
 exit
